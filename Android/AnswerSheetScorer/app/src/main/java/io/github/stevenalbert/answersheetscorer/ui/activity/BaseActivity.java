ce7a4a2d62647833d6068f978ef3a21f3d6618b2
Steven Albert <steven.a.540@hotmail.com>
2018-06-12 22:06:04 +0700

    Initial commit
    - Initial commit of Android Project

---
+++ Android/AnswerSheetScorer/app/src/main/java/io/github/stevenalbert/answersheetscorer/ui/activity/BaseActivity.java
@@ -0,0 +1,12 @@
+package io.github.stevenalbert.answersheetscorer.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Steven Albert on 6/12/2018.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    private static final String TAG = BaseActivity.class.getSimpleName();
+
+}
---
+++ Android/AnswerSheetScorer/app/src/main/java/io/github/stevenalbert/answersheetscorer/ui/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package io.github.stevenalbert.answersheetscorer.ui.activity;
+
+import android.os.Bundle;
+
+import io.github.stevenalbert.answersheetscorer.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
